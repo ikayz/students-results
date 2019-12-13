be6a6aa85ee2012e8cd744ab6a1bf526abb8b913
ikayz <ikayz@live.com>
2019-12-13 10:08:53 +0200

    Add vector icons

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View, Button, FlatList} from 'react-native';
+import {Text, StyleSheet, View, Button, FlatList, TouchableHighlight} from 'react-native';
 import {getStudents} from '../actions';
 import {connect} from 'react-redux';
 import _ from 'lodash';
@@ -15,15 +15,25 @@ class Students extends Component {
       <View style={styles.container}>
         <Text> Retrieve Students Results </Text>
         <FlatList style={{width: '100%'}}
+        showsVerticalScrollIndicator={false}
         data={this.props.listStudent} keyExtractor={(item) => item.key}
         renderItem={({item}) => {
           return (
-            <View>
-              <Text>
-                {item.name},{item.address}
-              </Text>
+            <View style={styles.viewStyle}>
+              <Text style={styles.textStyle}>Name: {item.name}</Text>
+              <Text style={styles.textView2}>Age: {item.age}</Text>
+              <Text style={styles.textView2}>Address: {item.address}</Text>
+              <Text style={styles.textView2}>Gender: {item.gender}</Text>
+              <Text style={styles.textView2}>Subjects: {item.subjects}</Text>
+              <View>
+                <TouchableHighlight>
+                  <View>
+                    <Icon size={30} color="#fff" name="rocket" />
+                  </View>
+                </TouchableHighlight>
+              </View>
             </View>
-          )
+          );
         }}
         />
       </View>
@@ -37,7 +47,24 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff',
-  }
+    padding: 5,
+  },
+  viewStyle: {
+    elevation: 6,
+    borderRadius: 4,
+    backgroundColor: '#069',
+    padding: 15,
+    marginBottom: 10,
+  },
+  textStyle: {
+    fontSize: 20,
+    lineHeight: 28,
+    color: '#fff',
+  },
+  textView2: {
+    fontSize: 18,
+    color: '#fff',
+  },
 });
 
 function mapStateToProps(state) {
